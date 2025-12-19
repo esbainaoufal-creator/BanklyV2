71288c81a898481fd842561b114071e869e8a16c
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-19 11:32:07 +0100

    database config

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$dbname = "banklytwo";
+$user = "root";
+$pass = "rif50";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données.");
+}
